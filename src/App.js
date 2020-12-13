ff31de99f483ecde5d724b9b83d62a7854e17874
acoss22 <anacseq@gmail.com>
2020-12-13 21:28:16 +0000

    new counter react js app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    this.handleIncrementation = this.handleIncrementation.bind(this);
+    this.handleDecrementation = this.handleDecrementation.bind(this);
+  }
+
+  handleDecrementation() {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+  handleIncrementation() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return <div>
+      <button onClick={this.handleDecrementation}>-</button>
+      <button onClick={this.handleIncrementation}>+</button>
+      <span>{this.state.counter}</span>
+    </div>;
+  }
 }
 
 export default App;
